e97bbec988c1bb070da5a1cdb212264a65566047
Andrés Jordán <ajordanp@udd.cl>
2025-04-09 16:56:49 -0400

    Fix rutas con base para GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: '/', // 👈 nombre exacto del repositorio
+  base: '/Landing-asistencias-unidad/',
   plugins: [
     react(),
     tailwindcss(),
